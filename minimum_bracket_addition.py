76ac8f0fef66c6d5b8f03951e4969218f5c88e36
brandondelpozo <branzzel@gmail.com>
2021-10-15 01:40:52 -0500

    create minimum_bracket_addition.py

---
+++ minimum_bracket_addition.py
@@ -0,0 +1,24 @@
+"""
+Minimum Bracket Addition
+
+Easy
+
+Given a string s containing brackets ( and ), return the minimum number of brackets that can be inserted so that the brackets are balanced.
+Minimum Bracket Addition
+Constraints
+
+    n ≤ 100,000 where n is the length of s
+
+Example 1
+Input
+
+s = ")))(("
+
+Output
+
+5
+
+Explanation
+
+We can insert ((( to the front and )) to the end
+"""
