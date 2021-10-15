7521eee10a3fd6fd01fe5db349fcd64887bca1b4
brandondelpozo <branzzel@gmail.com>
2021-10-15 02:46:48 -0500

    update minimum_bracket_addition.py

--- minimum_bracket_addition.py
+++ minimum_bracket_addition.py
@@ -13,7 +13,7 @@ Example 1
 Input
 
 s = ")))(("
-
+s1 = "))(()"
 Output
 
 5
@@ -22,3 +22,16 @@ Explanation
 
 We can insert ((( to the front and )) to the end
 """
+
+class Solution:
+    def solve(self, s):
+        cnt1, cnt2 = 0, 0
+        for char in s:
+            if char == "(":
+                cnt1 += 1
+            else:
+                if cnt1:
+                    cnt1 -= 1
+                else:
+                    cnt2 += 1
+        return cnt1 + cnt2
